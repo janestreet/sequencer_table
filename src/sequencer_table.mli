6b5be10325b08c40d945680516a5972e8418de80
public-release <as-public-release@janestreet.com>
2025-04-27 21:24:24 +0100

    v0.18~preview.130.26+1192

--- src/sequencer_table.mli
+++ src/sequencer_table.mli
@@ -11,16 +11,16 @@
 
     It allows one to run jobs that are indexed by a key, while allowing jobs with distinct
     keys to run simultaneously, and ensuring that for any given key, at most one job with
-    that key is running at a time.  A sequencer table maintains optional state for each
+    that key is running at a time. A sequencer table maintains optional state for each
     key, and supplies that state to each running job indexed by the key.
 
     The implementation of a sequencer table is optimized for having a large number of keys
-    with state, while only a few keys with active jobs at any given time.  So, it is
+    with state, while only a few keys with active jobs at any given time. So, it is
     implemented with two tables, one of states and one of sequencers:
 
     {[
       { states : 'a Key.Table.t
-      ; jobs   : ('a option -> unit Deferred.t) Sequencer.t Key.Table.t
+      ; jobs : ('a option -> unit Deferred.t) Sequencer.t Key.Table.t
       }
     ]}
 
@@ -28,8 +28,8 @@
     when one adds a job, and automatically removes a sequencer from the [jobs] table
     whenever the sequencer has no jobs to run.
 
-    The implementation does not automatically release state; one must call [set_state t
-    ~key None]. *)
+    The implementation does not automatically release state; one must call
+    [set_state t ~key None]. *)
 
 open! Core
 open! Async_kernel
@@ -39,23 +39,22 @@ module Make (Key : sig
     type t [@@deriving sexp_of, hash, compare]
   end) : sig
   (** Every [Key.t] in the table has an associated [state], which each job running on that
-      key gets access to.  Jobs maybe have an associated [job_tag] which is provided
-      purely to assist debugging, as the tag is included in the sexp serialization of
-      [t]. *)
+      key gets access to. Jobs maybe have an associated [job_tag] which is provided purely
+      to assist debugging, as the tag is included in the sexp serialization of [t]. *)
   type ('state, 'job_tag) t [@@deriving sexp_of]
 
   val create : unit -> _ t
 
-  (** [enqueue t ~key f] enqueues [f] for [key].  [f] will be called with the state of
+  (** [enqueue t ~key f] enqueues [f] for [key]. [f] will be called with the state of
       [key] when invoked.
 
       Invariant 1: it is guaranteed that [f] will not be called immediately.
 
       Invariant 2: if [f] raises, then the exception will be raised to the monitor in
-      effect when [enqueue] was called.  Subsequent jobs for [key] will proceed.
+      effect when [enqueue] was called. Subsequent jobs for [key] will proceed.
 
       Invariant 3: to avoid race, there are no deferred operations between finding the
-      state and calling [f] with the state found.  Otherwise, the user would need to
+      state and calling [f] with the state found. Otherwise, the user would need to
       consider the race that the state passed to [f] might have been changed by
       [set_state]. *)
   val enqueue
@@ -65,7 +64,7 @@ module Make (Key : sig
     -> ('state option -> 'b Deferred.t)
     -> 'b Deferred.t
 
-  (** [set_state t key state_opt] sets the state for [key] immediately.  The state will be
+  (** [set_state t key state_opt] sets the state for [key] immediately. The state will be
       kept internally until set to [None] *)
   val set_state : ('state, _) t -> key:Key.t -> 'state option -> unit
 
@@ -84,7 +83,7 @@ module Make (Key : sig
 
   val exists : ('state, _) t -> f:(Key.t -> 'state option -> bool) -> bool
 
-  (** The result is determined when all jobs enqueued before this are finished.  The
+  (** The result is determined when all jobs enqueued before this are finished. The
       implementation adds a new job to every key currently with at least one running job
       attached, so it will affect [num_unfinished_jobs] *)
   val prior_jobs_done : (_, _) t -> unit Deferred.t
